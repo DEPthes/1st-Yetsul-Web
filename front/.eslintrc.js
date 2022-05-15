f9c474eb906e795131ba14f2d0db4b8bab7d109d
ksone02 <eksovus@gmail.com>
2022-05-15 19:31:02 +0900

    [chore] CRA, Prettier, Eslint 설정 변경

---
+++ front/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		// import 관련 규칙 모음
+
+		"prettier",
+		"airbnb",
+		"plugin:prettier/recommended",
+		// prettier 관련 규칙 모음
+	],
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	plugins: ["prettier"],
+	rules: {
+		// 추가하고 싶은 rule을 더 추가해줍니다.
+		"linebreak-style": 0,
+		"import/prefer-default-export": 0,
+		"prettier/prettier": 0,
+		"import/extensions": 0,
+		"no-use-before-define": 0,
+		"no-shadow": 0,
+		"react/prop-types": 0,
+		"jsx-a11y/no-noninteractive-element-interactions": 0,
+		"react/jsx-props-no-spreading": "off",
+		"no-console": "off",
+		"import/no-unresolved": 0,
+		"import/no-extraneous-dependencies": 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+		"react/jsx-filename-extension": [2, { extensions: [".js", ".jsx"] }],
+	},
+};
---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="main">
+			<header>DEPth 프론트엔드 초기 세팅</header>
+			<main>
+				<h1>프론트 세팅 완료</h1>
+				<p>프로젝트 같이 잘 해봐용</p>
+			</main>
+			<footer>DEPth, 2022</footer>
+		</div>
+	);
+}
+
+export default App;
