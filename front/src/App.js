98e9a24f3f441a443b3c520a0d0f83ee4d09639e
ksone02 <eksovus@gmail.com>
2022-05-15 20:37:27 +0900

    [chore] S3 TEST 2

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header>DEPth 프론트엔드 초기 세팅</header>
 			<main>
 				<h1>프론트 세팅 완료</h1>
-				<p>프로젝트 같이 잘 해봐요!!</p>
+				<p>프로젝트 같이 잘 해봐요!!!</p>
 			</main>
 			<footer>DEPth, 2022</footer>
 		</div>
