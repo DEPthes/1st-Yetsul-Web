95ffbb497773a436d5a7e98a865eb268776506cc
LEE BO HYUN <frombozztoang@gmail.com>
2022-05-26 14:37:59 +0900

    [feat] 술bti 결과 화면

--- front/src/App.js
+++ front/src/App.js
@@ -1,35 +1,18 @@
 import React from "react";
-import styled from "@emotion/styled";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Alcoholresult from "./components/alcoholtest/alcoholresult";
 
-const Sangwon = styled.header`
-	width: 100%;
-	height: 100px;
-	background-color: #60666b;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	font-size: 35px;
-	font-weight: 700;
-	color: white;
-`;
-
-const H1text = styled.h1`
-	color: red;
-`;
+import Home from "./Home";
 
 function App() {
 	return (
-		<div className="main">
-			<Sangwon>여기는 헤더</Sangwon>
-			<main>
-				<H1text>프론트 세팅 완료</H1text>
-				<p>프로젝트 같이 잘 해봐요!!!</p>
-			</main>
-			<Alcoholresult />
-			<footer>DEPth, 2022</footer>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/test" element={<Alcoholresult />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
