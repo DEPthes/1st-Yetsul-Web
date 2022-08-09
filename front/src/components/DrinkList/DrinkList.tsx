34780676ce2f3912cd698908e266f0e00dd6bbf8
LEE BO HYUN <frombozztoang@gmail.com>
2022-08-09 09:54:34 +0900

    [feat] pagination

--- front/src/components/DrinkList/DrinkList.tsx
+++ front/src/components/DrinkList/DrinkList.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import BackgroundTemplate from '../common/BackgroundTemplate';
 import DrinkCategoryBtn from './DrinkCategoryBtn';
 import DrinkListElement from './DrinkListElement';
 import { drinkInfo } from '../common/dummydata';
+import Pagination from './Pagination';
 
 const DrinkList: React.FC = () => {
+    const [drinks, setDrinks] = useState<DrinkType[]>([]);
+    const [limit, setLimit] = useState(12); // 페이지 당 보여줄 게시물 수
+    const [page, setPage] = useState(1); // 현재 페이지
+    const offset = (page - 1) * limit; // 페이지 당 첫 게시물의 index
+
+    useEffect(() => {
+        axios
+            .post(`https://depth-server.herokuapp.com/alcohol/list`, {
+                filter: 'ASC',
+            })
+            .then((res) => setDrinks(res.data))
+            .catch((err) => console.log(err));
+    });
+
     return (
         <BackgroundTemplate heightValue="auto">
             <Inner>
@@ -35,24 +51,33 @@ const DrinkList: React.FC = () => {
                             </li>
                         </ul>
                     </Category>
+
                     <DrinkElList>
                         <ul>
-                            {drinkInfo.map((data) => {
-                                return (
-                                    <li key={data.id}>
-                                        <DrinkListElement
-                                            img={data.alcoholImage}
-                                            name={data.AlcoholName}
-                                            abv={data.AlcoholByVolume}
-                                            price={data.price}
-                                        />
-                                    </li>
-                                );
-                            })}
+                            {drinks
+                                .slice(offset, offset + limit)
+                                .map((drink) => {
+                                    return (
+                                        <li key={drink.id}>
+                                            <DrinkListElement
+                                                img={drink.alcoholImage}
+                                                name={drink.AlcoholName}
+                                                abv={drink.AlcoholByVolume}
+                                                price={drink.price}
+                                            />
+                                        </li>
+                                    );
+                                })}
                         </ul>
                     </DrinkElList>
                 </Content>
             </Inner>
+            <Pagination
+                total={drinks.length}
+                limit={limit}
+                page={page}
+                setPage={setPage}
+            />
         </BackgroundTemplate>
     );
 };
@@ -114,3 +139,22 @@ const DrinkElList = styled.div`
         }
     }
 `;
+
+type DrinkType = {
+    children: ReactNode;
+    id: number;
+    AlcoholName: string;
+    category: number;
+    brewery: string;
+    price: number;
+    AlcoholByVolume: number;
+    sweet: boolean;
+    bitter: boolean;
+    refreshing: boolean;
+    clean: boolean;
+    cool: boolean;
+    sour: boolean;
+    description: string;
+    star: number;
+    alcoholImage: string;
+};
