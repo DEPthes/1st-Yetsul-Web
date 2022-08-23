e69b53219856082119b5c34319f3bda8c07ee311
LEE BO HYUN <frombozztoang@gmail.com>
2022-08-24 00:51:08 +0900

    [chore] router 수정

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -6,12 +6,13 @@ import Header from './components/common/Header';
 import store from './store/config';
 import Depth from './components/introduction/depth';
 import Service from './components/introduction/service';
-import Detail from './components/Detail/DrinkDetail';
+
 import DrinkList from './components/DrinkList/DrinkList';
 import SoolotMachine from './components/soolotmachine/SoolotMachine';
 import DrinkTestBox from './components/DrinkTest/DrinkTest';
 import DrinkTestResult from './components/DrinkTest/DrinkTestResult';
 import MonthDrink from './components/MonthDrink/MonthDrink';
+import DrinkDetail from './components/Detail/DrinkDetail';
 
 const Router: React.FC = () => {
     return (
@@ -23,7 +24,8 @@ const Router: React.FC = () => {
                     <Route path="/auth/:service/callback" element={<App />} />
                     <Route path="/depth" element={<Depth />} />
                     <Route path="/service" element={<Service />} />
-                    <Route path="/list/:id" element={<Detail />} />
+
+                    <Route path="/list/:id/spec" element={<DrinkDetail />} />
                     <Route path="/list" element={<DrinkList />} />
                     <Route path="/month" element={<MonthDrink />} />
                     <Route path="/soolot" element={<SoolotMachine />} />
