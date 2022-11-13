d93502ac0bb70cdf205bdd775a9ee4bb4c0a10d2
rlotr02 <0314kelee@naver.com>
2022-11-13 23:28:20 +0900

    [Feat] 반응형 수정

--- front/src/components/DrinkTestShare/ShareKakaoBtn.tsx
+++ front/src/components/DrinkTestShare/ShareKakaoBtn.tsx
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ShareKakao from './ShareKakao';
 
 const ShareKakaoBtn: React.FC = () => {
+    const [shareButton, setShareButton] = useState(false);
+
     useEffect(() => {
         const script = document.createElement('script');
         script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
         script.async = true;
-
         document.body.appendChild(script);
+
+        script.onload = () => {
+            setShareButton(true);
+        };
 
         return () => {
             document.body.removeChild(script);
         };
     }, []);
-
-    return <ShareKakao />;
+    return <div>{shareButton === true ? <ShareKakao /> : null}</div>;
 };
 
 export default ShareKakaoBtn;
